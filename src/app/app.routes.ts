7e8137986198e917594773cd39b6e4cfdeb33e88
Vinicasagrande87 <viniciustst7@gmail.com>
2026-05-19 21:48:28 -0300

    ajuste nos caminhos das rotas e imports

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Vitrine } from '../app/pages/vitrine/vitrine';
+import { ProdutoDetalhe } from '../app/pages/produto-detalhe/produto-detalhe';
+import { Carrinho } from '../app/pages/carrinho/carrinho';
+import { NaoEncontrada } from '../app/pages/nao-encontrada/nao-encontrada';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'vitrine', pathMatch: 'full' },
+  { path: 'vitrine', component: Vitrine },
+  { path: 'carrinho', component: Carrinho },
+  { path: 'produto-detalhe/:id', component: ProdutoDetalhe },
+  { path: '**', component: NaoEncontrada }
+];
